9fc9dddb13decd1a75e87401230419b2a7b64db5
Sandra Barron <sandra@Sandras-MBP.lan>
2023-06-14 12:34:16 -0500

    added week 1

---
+++ Week 1/Assesment.py
@@ -0,0 +1,17 @@
+# Assesment Week 1 
+
+print(15+5+(3*2)/4**2+(3-7)*7)
+
+
+# Enter code here:
+print(86400 * 7)
+
+# Should print 604800
+
+
+total_computers = 30*20
+replacement_cycle = 5
+computers_per_year = total_computers / replacement_cycle
+
+
+print(computers_per_year) # Should print 120.0
---
+++ Week 1/Quiz1.py
@@ -0,0 +1,7 @@
+# Quiz 1 
+
+print("Programming in Python is fun!")
+
+
+ratio = (1 + (5 ** 0.5)) / 2
+print(ratio)
