e376d99576fa1ed9eea47a46bbfb7b039ed93624
Sandra Barron <sandra@Sandras-MBP.lan>
2023-06-28 13:28:51 -0500

    added Practice Quiz 2 from Week 3

---
+++ Week 3/PracticeQuiz2.py
@@ -0,0 +1,46 @@
+# Practice Quiz "For Loops" Week 3
+
+for n in range(6,18+1,3):
+    print(n*2)
+# Should print 12, 18, 24, 30, 36
+
+for n in range(19):
+    if n % 2 == 0:
+        print(n)
+
+for n in range(10):
+    print(n+n)
+# Should print all even numbers from 0 to 18
+
+for x in range(1,10+1):
+  print(x**3)
+# Should print 
+# 1
+# 8
+# 27
+# 64
+# 125
+#2 16
+# 343
+# 512
+# 729
+# 1000
+
+for n in range(0, 100, 7): 
+    print(n)
+# Should print 
+# 0
+# 7
+# 14
+# 21
+# 28
+# 35
+# 42
+# 49
+# 56
+# 63
+# 70
+# 77
+# 84
+# 91
+# 98
