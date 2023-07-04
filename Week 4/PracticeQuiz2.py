7703f53a6832d3a4772aac0682fabb263a73cef1
Sandra Barron <sandra@Sandras-MBP.lan>
2023-07-04 14:37:36 -0500

    added practice quiz lists week 4

---
+++ Week 4/PracticeQuiz2.py
@@ -0,0 +1,46 @@
+# Practice Quiz 2 "Lists" Week 4
+
+filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
+# Generate newfilenames as a list containing the new filenames
+# using as many lines of code as your chosen method requires.
+newfilenames = ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]  
+
+print(newfilenames) 
+# Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
+
+def pig_latin(text):
+  say = ""
+  # Separate the text into words
+  words = text.split()
+  for word in words:
+    # Create the pig latin word and add it to the list
+    pig_word = word[1:] + word[0] + "ay"
+    say += pig_word + " "
+    # Turn the list back into a phrase
+  return say.strip()
+		
+print(pig_latin("hello how are you")) # Should be "ellohay owhay reaay ouyay"
+print(pig_latin("programming in python is fun")) # Should be "rogrammingpay niay ythonpay siay unfay"
+
+def group_list(group, users):
+  members = ", ".join(users)
+  return group + ": " + members
+
+print(group_list("Marketing", ["Mike", "Karen", "Jake", "Tasha"])) # Should be "Marketing: Mike, Karen, Jake, Tasha"
+print(group_list("Engineering", ["Kim", "Jay", "Tom"])) # Should be "Engineering: Kim, Jay, Tom"
+print(group_list("Users", "")) # Should be "Users:"
+
+def guest_list(guests):
+	for guest in guests:
+		name, age, occupation = guest
+		print("{} is {} years old and works as {}".format(name, age, occupation))
+
+guest_list([('Ken', 30, "Chef"), ("Pat", 35, 'Lawyer'), ('Amanda', 25, "Engineer")])
+
+#Click Run to submit code
+"""
+Output should match:
+Ken is 30 years old and works as Chef
+Pat is 35 years old and works as Lawyer
+Amanda is 25 years old and works as Engineer
+"""
